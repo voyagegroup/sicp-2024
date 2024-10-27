1bf412c8fc347c53e7c49a832077c97d3eab1b44
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2024-10-27 22:45:30 +0900

    update

--- Chap1/1.20/noza-answer.rkt
+++ Chap1/1.20/noza-answer.rkt
@@ -6,19 +6,19 @@
 ; gcd(206, 40)
 ; -> (if (= 40 0) 206 (gcd 40 (remainder 206 40)))
 ; -> (gcd 40 (remainder 206 40))
-; -> (if (= (remainder 206 40) 0)
+; -> (if (= (remainder 206 40) 0); 1
 ;      40
 ;      (gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
 ; -> (if (= (remainder 40 (remainder 206 40)) 0)
 ;      (remainder 206 40)
-;      (gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))); 2
+;      (gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))); 3
 ; -> (gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
-; -> (if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0); 6
+; -> (if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0); 7
 ;      (remainder 40 (remainder 206 40))
-;      (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))); 6
+;      (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))));
 ; -> (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
-; -> (if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0); 13
+; -> (if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0); 14
 ;      (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
 ;      (gcd (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) (remainder (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))))
-; -> (remainder (remainder 206 40) (remainder 40 (remainder 206 40))); 17
+; -> (remainder (remainder 206 40) (remainder 40 (remainder 206 40))); 18
 ; -> 2
