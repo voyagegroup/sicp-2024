358f3a988943e1f9ea4eb81046b4dfac4237be6c
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2024-10-28 22:10:23 +0900

    ステップ数の計算を出す

--- Chap1/1.22/noza-answer.rkt
+++ Chap1/1.22/noza-answer.rkt
@@ -5,12 +5,15 @@
 (define (square x) (* x x))
 
 (define (smallest-divisor n)
-  (find-divisor n 2))
+  (find-divisor n 2 1))
 
-(define (find-divisor n test-divisor)
+(define (find-divisor n test-divisor count)
+  (display "count: ")
+  (display count)
+  (newline)
   (cond ((> (square test-divisor) n) n)
     ((divides? test-divisor n) test-divisor)
-    (else (find-divisor n (+ test-divisor 1)))))
+    (else (find-divisor n (+ test-divisor 1) (+ count 1)))))
 
 (define (divides? a b)
   (= (remainder b a) 0))
@@ -44,21 +47,26 @@
       (timed-prime-test st)
       (search-prime (+ st 2) end))))
 
-(search-prime 1001 1101)
-(search-prime 10001 10101)
-(search-prime 100001 100101)
-(search-prime 1000001 1000101)
+;(search-prime 1001 1101)
+;(search-prime 10001 10101)
+;(search-prime 100001 100101)
+;(search-prime 1000001 1000101)
 
-; 1009 *** 0.00048828125
-; 1013 *** 0.00048828125
-; 1019 *** 0.000732421875
+;1009 *** 0.0009765625
+;1013 *** 0.00048828125
+;1019 *** 0.00048828125
 
-;10007 *** 0.001220703125
-;10009 *** 0.001708984375
-; 10037 *** 0.00146484375
+;10007 *** 0.00146484375
+;10009 *** 0.001953125
+;10037 *** 0.001708984375
 
-;100003 *** 0.00390625
-;100019 *** 0.00390625
-;100043 *** 0.003662109375
+;100003 *** 0.00439453125
+;100019 *** 0.004638671875
+;100043 *** 0.00732421875
 
+;1000003 *** 0.012451171875
+;1000033 *** 0.0126953125
+;1000037 *** 0.012451171875
 
+(search-prime 1000001 1000004)
+; count: 1000
--- Chap1/1.23/noza-answer.rkt
+++ Chap1/1.23/noza-answer.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
+(define (square x) (* x x))
+
+(define (runtime) (current-inexact-milliseconds))
+
 ; 本文のコピー
 (define (smallest-divisor n)
   (find-divisor n 2))
@@ -13,22 +17,22 @@
 (define (divides? a b)
   (= (remainder b a) 0))
 
-(define (prime? n)
-  (= n (smallest-divisor n)))
-
 ; 問題
 (define (next x)
   (if (= x 2) 3 (+ x 2)))
 
 (define (smallest-divisor-ext n)
-  (find-divisor-ext n 2))
+  (find-divisor-ext n 2 1))
 
 
 ; 問題1.22のコピー
-(define (find-divisor-ext n test-divisor)
+(define (find-divisor-ext n test-divisor count)
+  (display "count: ")
+  (display count)
+  (newline)
   (cond ((> (square test-divisor) n) n)
     ((divides? test-divisor n) test-divisor)
-    (else (find-divisor-ext n (next test-divisor)))))
+    (else (find-divisor-ext n (next test-divisor) (+ count 1)))))
 
 (define (timed-prime-test n)
   (newline)
@@ -57,8 +61,26 @@
 (define (prime? n)
   (= n (smallest-divisor-ext n)))
 
-(search-prime 1001 1101)
-(search-prime 10001 10101)
-(search-prime 100001 100101)
-(search-prime 1000001 1000101)
+;(search-prime 1001 1101)
+;(search-prime 10001 10101)
+;(search-prime 100001 100101)
+;(search-prime 1000001 1000101)
+
+;1009 *** 0.000244140625
+;1013 *** 0.000244140625
+;1019 *** 0.000244140625
+
+;10007 *** 0.0009765625
+;10009 *** 0.0009765625
+;10037 *** 0.001220703125
+
+;100003 *** 0.002197265625
+;100019 *** 0.002197265625
+;100043 *** 0.002685546875
+
+;1000003 *** 0.0068359375
+;1000033 *** 0.0068359375
+;1000037 *** 0.007080078125
+
+(search-prime 1000003 1000004)
 
