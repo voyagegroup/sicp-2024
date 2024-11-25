7f29bc9064a852b22aa068d1952b59284e8b48f1
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2024-11-25 21:18:15 +0900

    1.35解いた

---
+++ Chap1/1.35/noza-answer.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+#| 本のfixed-point |#
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (func x) (+ 1 (/ 1 x)))
+
+(fixed-point func 1.0)
