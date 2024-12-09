fba4eac3d9103da8364859fc0034801778d5fd15
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2024-12-09 23:53:38 +0900

    1.45 updated

--- Chap1/1.45/noza-answer.rkt
+++ Chap1/1.45/noza-answer.rkt
@@ -30,7 +30,37 @@
 (define (average-damp f)
   (lambda (x) (average x (f x))))
 
-(define (n-root x n times)
-  (fixed-point (repeated (average-damp (lambda (y) (/ x (expt y (- n 1))))) times) 1.0))
+(define (n-root-times x n times)
+  (fixed-point ((repeated average-damp times) (lambda (y) (/ x (expt y (- n 1))))) 1.0))
 
-(n-root 16 4 10)
+#| (n-root 2 2 10) |#
+#| (n-root 3 2 10) |#
+#| (n-root 8 3 10) |#
+#| (n-root 27 3 10) |#
+#| (n-root 16 4 10) |#
+
+(n-root-times 2 2 1)
+(n-root-times 2 3 1)
+(n-root-times 2 4 2)
+(n-root-times 2 5 2)
+(n-root-times 2 6 2)
+(n-root-times 2 7 2)
+(n-root-times 2 8 3)
+(n-root-times 2 9 3)
+(n-root-times 2 10 3)
+(n-root-times 2 11 3)
+(n-root-times 2 12 3)
+(n-root-times 2 13 3)
+(n-root-times 2 14 3)
+(n-root-times 2 15 3)
+(n-root-times 2 16 4)
+
+(define (n-root x n)
+  (let ((times (floor (/ (log x) (log n)))))
+    (n-root-times x n times)))
+
+(n-root 2 2)
+(n-root 3 2)
+(n-root 8 3)
+(n-root 27 3)
+(n-root 16 4)
