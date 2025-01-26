67ece4e1a7fb7f68677fe674fa5b1961f049a3e6
shungenie <ca02291@cartahd.com>
2025-01-26 16:08:40 +0900

    fix

--- Chap2/2.13/shun.rkt
+++ Chap2/2.13/shun.rkt
@@ -1,10 +1,10 @@
 ; 区間xのパーセント相対誤差を割合に直したものをa、区間yのパーセント相対誤差を割合に直したものをbとする。
 ; この時区間x, yはそれぞれの中央値をxc, ycとすると、
 (define x
-  (make-interval (* xc (- 1 a)) (* xc (+ 1 a))))
+  (make-interval (- xc (/ (* xc a) 2)) (+ xc (/ (* xc a) 2))))
 
 (define y
-  (make-interval (* yc (- 1 b)) (* yc (+ 1 b))))
+  (make-interval (- yc (/ (* yc b) 2)) (+ yc (/ (* yc b) 2))))
 
 ; 全てを正とすると、掛け算の式は
 (define (mul-interval x y)
@@ -19,10 +19,10 @@
 これにx, yの定義を当てはめると、
 
 (define (mul-interval x y)
-  (let ((p1 (* (* xc (- 1 a)) (* yc (- 1 b))))
-        (p2 (* (* xc (- 1 a)) (* yc (+ 1 b))))
-        (p3 (* (* xc (+ 1 a)) (* yc (- 1 b))))
-        (p4 (* (* xc (+ 1 a)) (* yc (+ 1 b)))))
+  (let ((p1 (* (- xc (/ (* xc a) 2)) (- yc (/ (* yc b) 2))))
+        (p2 (* (- xc (/ (* xc a) 2)) (+ yc (/ (* yc b) 2))))
+        (p3 (* (+ xc (/ (* xc a) 2)) (- yc (/ (* yc b) 2))))
+        (p4 (* (+ xc (/ (* xc a) 2)) (+ yc (/ (* yc b) 2)))))
     (make-interval (min p1 p2 p3 p4)
                    (max p1 p2 p3 p4))))
 
