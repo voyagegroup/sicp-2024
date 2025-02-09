a882e87e17e712c237a6a78c43f18b20948d0130
shungenie <ca02291@cartahd.com>
2025-02-09 15:34:02 +0900

    fix

--- Chap2/2.18/shun.rkt
+++ Chap2/2.18/shun.rkt
@@ -1,20 +1,5 @@
 #lang racket
 
-(define (length items)
-  (if (null? items)
-      0
-      (+ 1 (length (cdr items)))))
-
-(define (list-ref items n)
-  (if (= n 0)
-      (car items)
-      (list-ref (cdr items) (- n 1))))
-
-(define (last-pair li)
-  (if (null? (cdr li))
-      (list (car li))
-      (last-pair (cdr li))))
-
 (define (reverse li)
   (define (reverse-iter result li)
     (if (null? li)
