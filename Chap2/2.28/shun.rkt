3eb2d90b0a55887e2567f07b6cbd9b3869a146f1
shungenie <ca02291@cartahd.com>
2025-02-15 16:38:39 +0900

    fix

---
+++ Chap2/2.28/shun.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (fringe tree)
+    (define (iter items result)
+      (if (null? items)
+          result
+          (if (pair? (car items))
+              (iter (cdr items) (append result (fringe (car items))))
+              (iter (cdr items) (append result (list (car items)))))))
+  (iter tree '()))
+
+(define x (list (list 1 (list 2)) (list 3 4)))
+
+(fringe x) 
+;'(1 2 3 4)
