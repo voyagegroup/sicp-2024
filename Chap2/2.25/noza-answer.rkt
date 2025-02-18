18b4294f11eccdb4c715fbbb8f7bde261ca2f481
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2025-02-18 09:49:09 +0900

    2.25

---
+++ Chap2/2.25/noza-answer.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(define (list3) (list (1 (2 (3 (4 (5 (6 7))))))))
+
+(car (cdr (car (cdr (cdr (list 1 3 (list 5 7) 9))))))
+(car (car (list (list 7))))
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7))))))))))))))))))
+
