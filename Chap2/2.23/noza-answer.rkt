b9b5d64ffac72d499697377fec688574fb83c792
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2025-02-18 09:08:04 +0900

    2.23

---
+++ Chap2/2.23/noza-answer.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (for-each proc items)
+  (if (null? items)
+      (void)
+      (begin (proc (car items))
+                   (for-each proc (cdr items)))))
+
+(for-each (lambda (x) (newline) (display x))
+          (list 57 321 88))
+
