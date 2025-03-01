62bf538d52f33892c2747ab24c687f68d974dd46
shungenie <ca02291@cartahd.com>
2025-03-01 20:50:32 +0900

    2.36

---
+++ Chap2/2.36/shun.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+; (map car seqs) seqs = (1 4 7 10)
+; (map cdr seqs)seqs  = ((2 3) (5 6) (8 9) (11 12))
+
+(define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+
+(accumulate-n + 0 s)
+; '(22 26 30)
