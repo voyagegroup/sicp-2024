c967e442384f1d62ae88670b314d5477fbfafda2
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2025-03-10 08:08:11 +0900

    2.39 解いた

---
+++ Chap2/2.39/noza.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(define (fold-right op initial sequence)
+ (if (null? sequence)
+     initial
+     (op (car sequence)
+         (fold-right op initial (cdr sequence)))))
+
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse-right sequence)
+ (fold-right (lambda (x y) (append y (list x))) null sequence))
+
+(define (reverse-left sequence)
+ (fold-left (lambda (x y) (cons y x)) null sequence))
+
+(reverse-right (list 1 2 3))
+
+(reverse-left (list 1 2 3))
