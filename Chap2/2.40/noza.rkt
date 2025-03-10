839b4e0f608278ca58357385243a31d19495aa31
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2025-03-11 08:11:03 +0900

    2.40 解いた

--- Chap2/2.40/noza.rkt
+++ Chap2/2.40/noza.rkt
@@ -21,3 +21,36 @@
            (enumerate-interval 1 n)))
 
 (unique-pairs 8)
+
+;; prime-sum-pairsの定義を簡単にせよ.
+
+; 必要なものを定義
+(define (square x) (* x x))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+; ここまで
+
+; 解答
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (unique-pairs n))))
+
+(prime-sum-pairs 6)
