56a77b2c2633af435e773dbd63de9d83f661d586
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2025-03-31 21:21:29 +0900

    2.55解いた

---
+++ Chap2/2.55/noza.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(car ''abracadabra)
+; => 'quote
+
+(car ''a)
+; => 'quote
+
+(display 'abracadabra)
+(newline)
+
+(display ''abracadabra)
+; => (quote abracadabra)
+(newline)
+
+(display '''abracadabra)
+; => (quote (quote abracadabra))
+(newline)
+
+; ''quote は (quote abracadabra) と評価されるため car で最初の要素を取得すると 'quote が表示される
