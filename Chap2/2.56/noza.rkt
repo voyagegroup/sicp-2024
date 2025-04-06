fc572e5e44eefacc363f1f890bfbfe45bbdcf7f4
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2025-04-06 09:38:11 +0900

    2.56解いた

--- Chap2/2.56/noza.rkt
+++ Chap2/2.56/noza.rkt
@@ -21,6 +21,7 @@
         ((=number? m1 1) m2)
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
@@ -36,6 +37,22 @@
 
 (define (multiplicand p) (caddr p))
 
+
+;; ここから解答
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base s) (cadr s))
+
+(define (exponent s) (caddr s))
+
+(define (make-exponentiation b e)
+  (cond ((=number? e 0) 1)
+        ((=number? e 1) b)
+        ((and (number? b) (number? e)) (expt b e))
+        (else (list '** b e))))
+
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp)
@@ -49,10 +66,22 @@
                          (deriv (multiplicand exp) var))
            (make-product (deriv (multiplier exp) var)
                          (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+          (exponent exp)
+          (make-product
+           (make-exponentiation
+            (base exp)
+            (- (exponent exp) 1))
+           (deriv (base exp) var))))
         (else
          (error "unknown expression type -- DERIV" exp))))
 
-;; ここまで
+;; 検算
 
-(define (exponentiation? x)
-  (and (pair? x) (eq? (car x) '**)))
+(deriv '(+ (** x 3)
+           (+ (* 5 (** x 2))
+              (+ (* 3 x)
+                 10)))
+       'x)
+
