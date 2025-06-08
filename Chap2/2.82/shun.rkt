5e250f2bb7bc0e2ea1c11181e893bb774d45621d
shungenie <ca02291@cartahd.com>
2025-06-09 00:40:50 +0900

    2.82...

---
+++ Chap2/2.82/shun.rkt
@@ -0,0 +1,279 @@
+#lang sicp
+
+(define coercion-list '())
+
+(define (clear-coercion-list)
+  (set! coercion-list '()))
+
+(define (put-coercion type1 type2 item)
+  (if (get-coercion type1 type2) coercion-list 
+      (set! coercion-list
+            (cons (list type1 type2 item)
+                  coercion-list))))
+
+(define (get-coercion type1 type2)
+  (define (get-type1 listItem)
+    (car listItem))
+  (define (get-type2 listItem)
+    (cadr listItem))
+  (define (get-item listItem)
+    (caddr listItem))
+  (define (get-coercion-iter list type1 type2)
+    (if (null? list) #f
+        (let ((top (car list)))
+          (if (and (equal? type1 (get-type1 top))
+                   (equal? type2 (get-type2 top))) (get-item top)
+                   (get-coercion-iter (cdr list) type1 type2)))))
+  (get-coercion-iter coercion-list type1 type2))
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+(define (attach-tag type-tag contents)
+  (if (eq? type-tag 'scheme-number)
+      contents
+      (cons type-tag contents)))
+
+(define (type-tag datum)
+  (cond
+    ((number? datum) 'scheme-number)
+    ((pair? datum) (car datum))
+    (else (error "Bad tagged datum -- TYPE-TAG" datum))))
+
+(define (contents datum)
+  (cond
+    ((number? datum) datum)
+    ((pair? datum) (cdr datum))
+    (else (error "Bad tagged datum -- CONTENTS" datum))))
+
+(define (square n)
+  (* n n))
+
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+(define (equ? x y)
+  (apply-generic 'equ? x y))
+(define (=zero? z)
+  (apply-generic '=zero? z))
+
+
+(define (install-rectangular-package)
+   ;; 内部手続き
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a) 
+    (cons (* r (cos a)) (* r (sin a))))
+  
+
+   ;; システムの他の部分とのインターフェース
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular 
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular 
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (install-polar-package)
+   ;; 内部手続き
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y) 
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+
+   ;; システムの他の部分とのインターフェース
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar 
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(install-rectangular-package)
+(install-polar-package)
+
+
+
+(define (install-complex-package)
+   ;; 直交座標と極座標パッケージから取り入れた手続き
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+
+   ;; 内部手続き
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+  (define (equ? z1 z2)
+    (and (= (real-part z1) (real-part z2)) (= (imag-part z1) (imag-part z2))))
+  (define (=zero? z)
+    (and (= (real-part z) 0) (= (imag-part z) 0)))
+
+   ;; システムの他の部分へのインターフェース
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+  (put 'equ? '(complex complex) equ?)
+  (put '=zero? '(complex) =zero?)
+  'done)
+
+; 複素数
+(install-complex-package)
+(define (make-complex-from-real-imag x y)
+  ((get 'make-from-real-imag 'complex) x y))
+ 
+(define (make-complex-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'complex) r a))
+
+
+(define (install-scheme-number-package)
+  (define (tag x)
+    (attach-tag 'scheme-number x))    
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number
+       (lambda (x) (tag x)))
+  (put 'equ? '(scheme-number scheme-number)
+       (lambda (x y) (tag (= x y))))
+  (put '=zero? '(scheme-number)
+       (lambda (n) (= n 0)))
+  (put 'exp '(scheme-number scheme-number)
+     (lambda (x y) (tag (expt x y)))) 
+  'done)
+
+(install-scheme-number-package)
+
+(define (scheme-number->complex n)
+  (make-complex-from-real-imag (contents n) 0))
+
+(put-coercion 'scheme-number 'complex scheme-number->complex)
+
+(define (scheme-number->scheme-number n) n)
+
+(define (complex->complex z) z)
+(put-coercion 'scheme-number 'scheme-number
+              scheme-number->scheme-number)
+(put-coercion 'complex 'complex complex->complex)
+
+(define (apply-generic op . args)
+  (define (try-coercion types)
+    (define (try-coercion-itr type i-args)
+      (if (null? i-args)
+          (apply apply-generic (cons op (map (lambda (x) ((get-coercion (type-tag x) type) x)) args)))
+          (let ((coe (get-coercion (type-tag (car i-args)) type)))
+            (cond
+              (coe (try-coercion-itr type (cdr i-args)))
+              (else (try-coercion (cdr types)))      
+                ))))
+    (if (null? types)
+        (error "No method for these types")
+        (try-coercion-itr (car types) args)
+        ))
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args)) ; ここが全然反応しない。procが見つからない。procが#tになる条件があるようにも思えないが、元々動かない？
+          (try-coercion type-tags)
+          ))))
+
+; 動かせなかった...
+
+(define z1 (make-complex-from-real-imag 3 4))  ; 3 + 4i
+(define z2 (make-complex-from-real-imag 1 2))  ; 1 + 2i
+
+(define result (apply-generic 'add z1 z2 z1))
+
+result
+; (complex rectangular 4 . 6)
+
+
+(define result-s (apply-generic 'add 3 4))
+
+result-s
+; 7
+
+(define result-m (apply-generic 'add z1 4))
+
+result-m
