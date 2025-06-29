7bafb631ca04ec6074eda8f0224ffebd61c041c7
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2025-06-29 15:47:05 +0900

    修正

--- Chap2/2.87/noza.rkt
+++ Chap2/2.87/noza.rkt
@@ -492,7 +492,7 @@
                    (else
                     (adjoin-term
                      (make-term (order t1)
-                                (+ (coeff t1) (coeff t2)))
+                                (add (coeff t1) (coeff t2)))
                      (add-terms (rest-terms L1)
                                 (rest-terms L2)))))))))
 
@@ -509,7 +509,7 @@
         (let ((t2 (first-term L)))
           (adjoin-term
            (make-term (+ (order t1) (order t2))
-                      (* (coeff t1) (coeff t2)))
+                      (mul (coeff t1) (coeff t2)))
            (mul-term-by-all-terms t1 (rest-terms L))))))
 
   ;; システムの他の部分とのインターフェース
