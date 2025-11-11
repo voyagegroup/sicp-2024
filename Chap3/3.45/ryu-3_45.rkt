190b83e29019917cfbaaf840d816e96721cbbdf6
Ishikawa <r.ishikawa@cartahd.com>
2025-11-11 14:05:30 +0900

    add 11/11分

---
+++ Chap3/3.45/ryu-3_45.rkt
@@ -0,0 +1,45 @@
+#lang sicp
+
+(define (make-account-and-serializer balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer)))
+    (define (dispatch m)
+;     元々            
+;     (cond ((eq? m 'withdraw) withdraw)
+;           ((eq? m 'deposit) deposit)
+
+      (cond ((eq? m 'withdraw) (balance-serializer withdraw))
+            ((eq? m 'deposit) (balance-serializer deposit))
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+
+(define (deposit account amount)
+ ((account 'deposit) amount))
+
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
+
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange))
+     account1
+     account2)))
+
+#|
+serialized-exchange内でlockを取るのに、その内側のexchange→withdrawでもlockが必要になるので、デッドロックになってしまう。
+|#
