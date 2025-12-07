a06c3a26852347619d80c2c32b544e6e5f040bc8
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2025-12-07 22:55:03 +0900

    解いた

---
+++ Chap3/3.56/noza.rkt
@@ -0,0 +1,54 @@
+#lang sicp
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ a b) (cons a (delay b)))))
+
+(define the-empty-stream '())
+(define (stream-null? s) (null? s))
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+;; 解答
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (cons-stream s1car (merge (stream-cdr s1) s2)))
+                 ((> s1car s2car)
+                  (cons-stream s2car (merge s1 (stream-cdr s2))))
+                 (else
+                  (cons-stream s1car
+                               (merge (stream-cdr s1)
+                                      (stream-cdr s2)))))))))
+
+(define S
+  (cons-stream 1
+               (merge (merge (scale-stream S 2)
+                             (scale-stream S 3))
+                      (scale-stream S 5))))
+
+;; take
+(define (stream-take s k)
+  (if (or (zero? k) (stream-null? s))
+      '()
+      (cons (stream-car s)
+            (stream-take (stream-cdr s) (- k 1)))))
+
+(stream-take S 20)
+;; => '(1 2 3 4 5 6 8 9 10 12 15 16 18 20 24 25 27 30 32 36)
